25a9a45ecb028c3315e29cbb4e78d0ee1496a378
529528 <529528@RB-203A-14>
2019-10-15 10:52:02 +0100

    added writeline test

--- CodeMagazineDemo/CodeMagazineDemo/Program.cs
+++ CodeMagazineDemo/CodeMagazineDemo/Program.cs
@@ -15,6 +15,7 @@ namespace CodeMagazineDemo
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
+            Console.WriteLine("This is a test");
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
